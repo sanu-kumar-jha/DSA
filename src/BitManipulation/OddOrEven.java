78b763f2761de44ec149b7852042ee2e8aefe43f
sanu-kumar-jha <82073575+sanu-kumar-jha@users.noreply.github.com>
2024-05-11 10:33:46 +0530

    OddOrEven

--- src/BitManipulation/OddOrEven.java
+++ src/BitManipulation/OddOrEven.java
@@ -1,5 +1,22 @@
 package BitManipulation;
 
-public class OddOrEven {
+import java.util.Scanner;
 
+class OddOrEven {
+
+    static boolean isEven(int n) {
+        if ((n ^ 1) == (n + 1))
+            return true;
+        else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        int n;
+        Scanner scannerObject = new Scanner(System.in);
+        System.out.println("Enter the value of n:");
+        n = scannerObject.nextInt();
+        System.out.print(isEven(n) == true ? "Even"
+                : "Odd");
+    }
 }
